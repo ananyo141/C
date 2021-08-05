11acd5e48c1def618ba9780e70651ac048171963
ananyo141 <ananyo141@gmail.com>
2021-08-05 14:16:05 +0530

    added gitignore rules and updated parts database

--- KN KING/Parts Database/inventory.c
+++ KN KING/Parts Database/inventory.c
@@ -7,14 +7,17 @@
 struct part inventory[MAX_PARTS];
 
 int main() {
-    char command[COMMAND_LEN + 1];
+    char command[COMMAND_LEN + 1]; // store the input command
+    void (*available_commands[])(void) = {insert, search, update, print}; // array of available function pointer
+    enum command_enum {INSERT, SEARCH, UPDATE, PRINT} command_val;
     for (;;) {
         printf("Enter Command: ");
         read_line(command, COMMAND_LEN);
-        if (strcmp(command, "insert") == 0) insert();
-        else if (strcmp(command, "search") == 0) search();
-        else if (strcmp(command, "update") == 0) update();
-        else if (strcmp(command, "print") == 0) print();
+
+        if (strcmp(command, "insert") == 0) available_commands[INSERT]();
+        else if (strcmp(command, "search") == 0) available_commands[SEARCH]();
+        else if (strcmp(command, "update") == 0) available_commands[UPDATE]();
+        else if (strcmp(command, "print") == 0) available_commands[PRINT]();
         else if (strcmp(command, "quit") == 0) return 0;
         else printf("Invalid command\n");
     }
