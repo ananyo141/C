a175b36ea839803df1bff828f06e2d1e7d11e393
ananyo141 <ananyo141@gmail.com>
2021-08-17 01:35:08 +0530

    added binary encryption for xorEncryption

--- KN KING/Provided Examples/xorEncryption.c
+++ KN KING/Provided Examples/xorEncryption.c
@@ -1,18 +1,35 @@
 // Use XOR Encryption to encode a message
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-#define KEY '&'
 
-int main() {
-    int orig_char, new_char;
-
-    while ((orig_char = getchar()) != EOF) {
-        new_char = orig_char ^ KEY;
-        if (isprint(orig_char) && isprint(new_char))
-            putchar(new_char);
-        else
-            putchar(orig_char);
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: xorEncryption <inputfile> <outputfile>\n");
+        exit(EXIT_FAILURE);
     }
+
+    int orig_char, key;
+    printf("Enter the key: ");
+    scanf("%c", &key);
+
+    FILE *readfile, *writefile;
+    if ((readfile = fopen(argv[1], "rb")) == NULL) {
+        printf("File %s couldn't be opened\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    if ((writefile = fopen(argv[2], "wb")) == NULL) {
+        printf("File %s couldn't be opened\n", argv[2]);
+        fclose(readfile);
+        exit(EXIT_FAILURE);
+    }
+
+    while ((orig_char = getc(readfile)) != EOF) {
+        putc(orig_char ^ key, writefile);
+    }
+    fclose(readfile);
+    fclose(writefile);
+    printf("File successfully encrypted/decrypted\n");
     
     return 0;
 }
