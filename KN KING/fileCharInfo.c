6043c9bfcb6bc033c7546768a115493771486c5f
ananyo141 <ananyo141@gmail.com>
2021-08-17 00:56:36 +0530

    fix bug fileCharInfo.c

--- KN KING/fileCharInfo.c
+++ KN KING/fileCharInfo.c
@@ -1,6 +1,7 @@
 // Display the properties of text file provided on the command line
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
 // TODO: Count the number of characters 
@@ -23,13 +24,17 @@ int main(int argc, char *argv[]) {
     }
     // Analyze the text file
     int ch;
+    char *spChar = " !@#$%%^&*()_+=-,./?'\"\\<>;";
     long ch_count = 0, word_count = 0, line_count = 0;
     while ((ch = getc(stream)) != EOF) {
-        if (ch == ' ' || ch == '.') {
-            while (ch == ' ' || ch == '.')
+        if (strchr(spChar, ch) != NULL) {
+            while (strchr(spChar, ch) != NULL)
                 ch = getc(stream);
-            ungetc(ch, stream);   // put back the non-space character for further reading
+            ungetc(ch, stream);   // put back the normal character for further reading
             word_count++;
+            if ((ch = getc(stream)) == EOF)
+                line_count++;
+            ungetc(ch, stream);
         }
         else if (isspace(ch)) 
             line_count++;
@@ -45,8 +50,8 @@ int main(int argc, char *argv[]) {
     printf("%15s%15ld\n", "Characters", ch_count);
     printf("%15s%15ld\n", "Words", word_count);
     printf("%15s%15ld\n", "Lines", line_count);
-    printf("%20s%10.2f\n", "Average Char/Word", ch_count/(float)word_count);
-    printf("%20s%10.2f\n", "Average Word/Line", word_count/(float)line_count);
+    printf("%15s%15.2f\n", "Char/Word", ch_count/(float)word_count);
+    printf("%15s%15.2f\n", "Word/Line", word_count/(float)line_count);
 
     return 0;
 }
