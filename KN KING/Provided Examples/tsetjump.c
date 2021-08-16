d6355d8990357b7c3f481284896193a5498d46eb
ananyo141 <ananyo141@gmail.com>
2021-08-16 16:18:54 +0530

    add tsetjump.c

---
+++ KN KING/Provided Examples/tsetjump.c
@@ -0,0 +1,33 @@
+// Testing setjmp and longjmp
+#include <stdio.h>
+#include <setjmp.h>
+
+jmp_buf env;
+
+void f1(void);
+void f2(void);
+
+int main() {
+    if (setjmp(env) == 0) 
+        printf("setjmp returned 0\n");
+    else {
+        printf("Program terminates: longjmp called\n");
+        return 0;
+    }
+
+    f1();
+    printf("Program terminates normally\n");
+    return 0;
+}
+
+void f1(void) {
+    printf("f1 begins\n");
+    f2();
+    printf("f1 returns\n");
+}
+
+void f2(void) {
+    printf("f2 begins\n");
+    longjmp(env, 1);
+    printf("f2 returns\n");
+}
